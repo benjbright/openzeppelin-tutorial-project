9a6d2a0b72af9574ace1ab3e64d614ee0fee8dbd
Ben <105577567+benjbright@users.noreply.github.com>
2022-07-10 08:59:40 +0100

    Project work
    Trying to fix bug when running index.js script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,6 +6,9 @@ async function main() {
   const box = await Box.deploy()
   await box.deployed()
   console.log(`Box deployed to: ${box.address}`)
+
+  const accounts = await ethers.provider.listAccounts()
+  console.log(accounts)
 }
 
 main()
@@ -17,5 +20,6 @@ main()
 
 // Note local blockchains do not persist their state - if closed then need to redeploy contract
 // Run the local blockchain in another terminal
-
-// 0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512
+// 0x5FbDB2315678afecb367f032d93F642f64180aa3
+// Need to explicitly set the network for hardhat to connect to
+// Otherwise will default to a new ephemeral network which Box contract wouldn't be deployed to
--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,18 @@
+const { ethers } = require("ethers")
+
 async function main() {
   // Retrieve accounts from the local node
   const accounts = await ethers.provider.listAccounts()
   console.log(accounts)
+
+  //   Set up an ethers contract, representing our deployed Box instance
+  const address = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+  const Box = await ethers.getContractFactory("Box")
+  const box = Box.attach(address)
+
+  //   Call the retrieve() function of the deployed Box contract
+  const value = await box.retrieve()
+  console.log(`Box value is: ${value.toString()}`)
 }
 
 main()
